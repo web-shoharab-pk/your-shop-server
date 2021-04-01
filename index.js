b60ed58f80ad0894242df6360537a3057da7a0a1
md shoharab pk <shoharabpk.2019@gmail.com>
2021-04-01 22:19:34 +0600

    yes

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ client.connect(err => {
             })
     })
     app.get('/orderDetailsInfo/:email', (req, res) => {
-        orderCollection.find(req.params.email)
+        orderCollection.find({"email":req.params.email} )
            .toArray((err, product) => {
                res.send(product)
            })
